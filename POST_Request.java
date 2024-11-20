1edfece73d30fb28995c762e3e3ef59945767ec4
unknown <harshirv@gmail.com>
2024-11-20 21:50:28 +0530

    Updated POST_Request.java to include correct endpoint

--- POST_Request.java
+++ POST_Request.java
@@ -11,7 +11,30 @@ public class POST_Request {
 	@Test
 	public void Create() {
 		RestAssured.baseURI = "https://petstore.swagger.io";
-		Response response = given().
+		Response response = given().header("content-type","application/json")
+				.log().all()
+				.body("{\r\n"
+						+ "  \"id\": 0,\r\n"
+						+ "  \"category\": {\r\n"
+						+ "    \"id\": 0,\r\n"
+						+ "    \"name\": \"string\"\r\n"
+						+ "  },\r\n"
+						+ "  \"name\": \"goldie\",\r\n"
+						+ "  \"photoUrls\": [\r\n"
+						+ "    \"string\"\r\n"
+						+ "  ],\r\n"
+						+ "  \"tags\": [\r\n"
+						+ "    {\r\n"
+						+ "      \"id\": 0,\r\n"
+						+ "      \"name\": \"string\"\r\n"
+						+ "    }\r\n"
+						+ "  ],\r\n"
+						+ "  \"status\": \"sold\"\r\n"
+						+ "}")
+				.when().post("/v2/pet").then().statusCode(200)
+				.log().body()
+				.extract().response();
+		System.out.println(response.asPrettyString());
 	}
 
 }
